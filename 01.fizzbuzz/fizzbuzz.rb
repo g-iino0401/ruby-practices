d7643f9eea094c90f6f38e3c3389e9ede1e92e6f
iino <g-iino@ruby-dev.jp>
2024-02-26 11:16:46 +0900

    インデント修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -3,13 +3,13 @@
 numbers = (1..20).to_a
 
 numbers.each do |number|
-    if number % 15 == 0
-        puts "FizzBuzz"
-    elsif number % 3 == 0
-        puts "Fizz"
-    elsif number % 5 == 0
-        puts "Buzz"
-    else 
-        puts number
-    end
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else 
+    puts number
+  end
 end
