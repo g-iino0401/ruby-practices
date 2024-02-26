5bb909016b3a584a86b5955170f8dace72ee669f
iino <g-iino@ruby-dev.jp>
2024-02-26 11:30:55 +0900

    インデント修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,30 +1,30 @@
 #!/usr/bin/env ruby
 
-#　カレンダーのプログラム
+# カレンダーのプログラム
 require 'date'
 require 'optparse'
 
 def calender(year, month)
-    # ヘッダー表示
-    puts "#{month}月 #{year}年".center(20)
-    puts "日 月 火 水 木 金 土"
+  # ヘッダー表示
+  puts "#{month}月 #{year}年".center(20)
+  puts "日 月 火 水 木 金 土"
 
-    # 最初の日
-    start_date = Date.new(year, month, 1)
+  # 最初の日
+  start_date = Date.new(year, month, 1)
 
-    # 最後の日
-    end_date = Date.new(year, month, -1)
+  # 最後の日
+  end_date = Date.new(year, month, -1)
 
-    # 月の1日が始まる位置までスペースを表示
-    print "   " * start_date.wday
+  # 月の1日が始まる位置までスペースを表示
+  print "   " * start_date.wday
 
-    # 日を1日から月末まで順に表示
-    (start_date..end_date).each do |date|
-        print date.day.to_s.rjust(2) + " "
+  # 日を1日から月末まで順に表示
+  (start_date..end_date).each do |date|
+    print date.day.to_s.rjust(2) + " "
     # 土曜日の場合は改行
-        puts if date.wday == 6
-    end
-    puts "\n"
+    puts if date.wday == 6
+  end
+  puts "\n"
 end
 
 options = {}
