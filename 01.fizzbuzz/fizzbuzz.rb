801c34bb15d4353ce51f4dcac5075b0743223631
iino <g-iino@ruby-dev.jp>
2024-02-26 10:10:52 +0900

    配列の記述方法を修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+numbers = (1..20).to_a
 
 numbers.each do |number|
     if number % 15 == 0
-    puts "FizzBuzz"
+        puts "FizzBuzz"
     elsif number % 3 == 0
         puts "Fizz"
     elsif number % 5 == 0
