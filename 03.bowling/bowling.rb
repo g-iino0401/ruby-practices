844f753524daccf71e71f2435b90209814fb4522
iino <g-iino@ruby-dev.jp>
2024-02-27 13:52:40 +0900

    命名と10フレーム目のif分修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,39 +2,40 @@
 # frozen_string_literal: true
 
 score = ARGV[0]
-scores = score.split(',')
+current_score = score.split(',')
 shots = []
-scores.each do |s|
-  if s == 'X'
+current_score.each do |score|
+  if score == 'X'
     shots << 10 # 1投目
     shots << 0 # 2投目
   else
-    shots << s.to_i
+    shots << score.to_i
   end
 end
 
 frames = []
-shots.each_slice(2) do |s|
-  frames << s
+shots.each_slice(2) do |shot|
+  frames << shot
 end
 
 point = 0
 frames.each_with_index do |frame, index|
-  point += if index < 9 # 1~9フレーム目の処理
-             if frame[0] == 10 # strike
-               if frames[index + 1][0] == 10 # 次のフレームもストライク
-                 20 + frames[index + 2][0]
-               else
-                 10 + frames[index + 1].sum
-               end
-             elsif frame.sum == 10 # spare
-               10 + frames[index + 1][0]
-             else
-               frame.sum
-             end
-           else # 10フレーム目の処理
-             frame.sum
-           end
-  # p "#{index + 1}フレーム目:#{frame}"
+  if index >= 9
+    point += frame.sum
+    next 
+  end
+
+  point += if frame[0] == 10 # strike
+      if frames[index + 1][0] == 10 # 次のフレームもストライク
+        20 + frames[index + 2][0]
+      else
+        10 + frames[index + 1].sum
+      end
+    elsif frame.sum == 10 # spare
+      10 + frames[index + 1][0]
+    else
+      frame.sum
+    end
+    # p "#{index + 1}フレーム目:#{frame}"
 end
 puts point
