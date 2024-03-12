7df15b09017f65b8fa4333c0fd383b211bfa1f21
iino <g-iino@ruby-dev.jp>
2024-03-12 09:48:54 +0900

    再修正

--- 04.ls/ls01.rb
+++ 04.ls/ls01.rb
@@ -1,23 +1,20 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+ROWS = 3
 
 def fetch_files
   Dir.glob('*')
 end
 
-ROWS = 3
 def display_files(files)
-
   file_count = files.size
   max_length = files.map(&:length).max
   columns = (file_count + ROWS - 1) / ROWS
   (0...columns).each do |col|
     (0...ROWS).each do |row|
       index = col + row * columns
-      if index < file_count
-        adjusted_filename = files[index].ljust(max_length + 1)
-        print adjusted_filename
-      end
+      next if index >= file_count
+      print files[index].ljust(max_length + 1)
     end
     puts
   end
